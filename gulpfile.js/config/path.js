4fbc2568d96c5c819c930b8a249d0f5f08b51376
Yaroslav-Bondar <yarobondmail@gmail.com>
2023-03-22 20:24:08 +0200

    feat: add normalize for css

--- gulpfile.js/config/path.js
+++ gulpfile.js/config/path.js
@@ -3,43 +3,41 @@ const pathDest = './public';
 
 module.exports = {
     root: pathDest,
-
     html: {
         src: pathSrc + '/html/*.html',
         watch: pathSrc + '/html/**/*.html',
         dest: pathDest,
     },
-
     pug: {
         src: pathSrc + '/pug/*.pug',
         watch: pathSrc + '/pug/**/*.pug',
         dest: pathDest,
     },
-
     css: {
         src: pathSrc + '/css/*.css',
         watch: pathSrc + '/css/**/*.css',
         dest: pathDest + '/css',
     },
-
+    normalize: {
+        src: pathSrc + '/css/normalize/{normalize,reset}.css',
+        watch: pathSrc + '/css/normalize/{normalize,reset}.css',
+        dest: pathDest + '/css/normalize',
+    },
     sass: {
         src: pathSrc + '/sass/*.{sass,scss}',
         watch: pathSrc + '/sass/**/*.{sass,scss}',
         dest: pathDest + '/css',
     },
-
     js: {
         src: pathSrc + '/js/*.js',
         watch: pathSrc + '/js/**/*.js',
         dest: pathDest + '/js',
     },
-
     img: {
         src: pathSrc + '/img/*.{png,jpg,jpeg,gif,svg}',
         watch: pathSrc + '/img/**/*.{png,jpg,jpeg,gif,svg}',
         dest: pathDest + '/img',
     },
-
     font: {
         src: pathSrc + "/font/*.{eot,ttf,otf,otc,ttc,woff,woff2,svg}",
         watch: pathSrc + "/font/**/*.{eot,ttf,otf,otc,ttc,woff,woff2,svg}",
